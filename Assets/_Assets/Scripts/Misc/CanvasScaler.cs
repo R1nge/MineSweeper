a459c40c7fee3afdeb0bb60e83c5b1a626ff292c
R1nge <r1ngegamedev@gmail.com>
2024-12-29 21:20:23 +0300

    feat(canvas scaler): move the canvas

--- Assets/_Assets/Scripts/Misc/CanvasScaler.cs
+++ Assets/_Assets/Scripts/Misc/CanvasScaler.cs
@@ -1,21 +1,51 @@
 ﻿using UnityEngine;
+using VContainer.Unity;
 
 namespace _Assets.Scripts.Misc
 {
-    public class CanvasScaler : MonoBehaviour
+    public class CanvasScaler : ITickable
     {
+        private readonly CameraHandler _cameraHandler;
+        private readonly Vector2 _maxDistanceFromCenter = new Vector2(100, 100);
         private bool _enabled = true;
+        private bool _moving;
         private RectTransform _rectTransform;
+        private Vector3 _startPosition;
 
-        public void Update()
+        private CanvasScaler(CameraHandler cameraHandler)
         {
-            if (Input.mouseScrollDelta.y > 0)
+            _cameraHandler = cameraHandler;
+        }
+
+        public void Tick()
+        {
+            if (!_moving)
             {
-                ZoomIn();
+                if (Input.mouseScrollDelta.y > 0)
+                {
+                    ZoomIn();
+                }
+                else if (Input.mouseScrollDelta.y < 0)
+                {
+                    ZoomOut();
+                }
             }
-            else if (Input.mouseScrollDelta.y < 0)
+
+            if (Input.GetMouseButtonDown(2))
             {
-                ZoomOut();
+                _moving = true;
+                _startPosition = _cameraHandler.Camera.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if (Input.GetMouseButton(2))
+            {
+                Move();
+            }
+
+            if (Input.GetMouseButtonUp(2))
+            {
+                _moving = false;
+                _startPosition = Vector3.zero;
             }
         }
 
@@ -25,6 +55,7 @@ namespace _Assets.Scripts.Misc
         }
 
         public void Enable() => _enabled = true;
+
         public void Disable() => _enabled = false;
 
         private void ZoomIn()
@@ -39,6 +70,18 @@ namespace _Assets.Scripts.Misc
             var scaleX = Mathf.Clamp(_rectTransform.localScale.x - 0.1f, 0.5f, 2f);
             var scaleY = Mathf.Clamp(_rectTransform.localScale.y - 0.1f, 0.5f, 2f);
             _rectTransform.localScale = new Vector2(scaleX, scaleY);
+        }
+
+        private void Move()
+        {
+            var delta = _startPosition - _cameraHandler.Camera.ScreenToWorldPoint(Input.mousePosition);
+            delta.z = 0;
+            _rectTransform.localPosition += delta;
+            var rectTransformPosition = _rectTransform.localPosition;
+            var scaledDistance = _maxDistanceFromCenter * _rectTransform.localScale;
+            rectTransformPosition.x = Mathf.Clamp(rectTransformPosition.x, -scaledDistance.x, scaledDistance.x);
+            rectTransformPosition.y = Mathf.Clamp(rectTransformPosition.y, -scaledDistance.y, scaledDistance.y);
+            _rectTransform.localPosition = rectTransformPosition;
         }
     }
 }
